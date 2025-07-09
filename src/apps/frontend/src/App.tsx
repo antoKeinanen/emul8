2c226ad9bcfab4b2e42e24362345c716be9e7b74
AntoKeinanen <anto.keinanen@gmail.com>
2025-07-09 15:31:39 +0300

    feat: proxy api with vite

--- src/apps/frontend/src/App.tsx
+++ src/apps/frontend/src/App.tsx
@@ -2,21 +2,24 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { useState } from "react";
 import { trpc } from "./lib/trpc";
 import { httpBatchLink } from "@trpc/client";
+import IndexPage from "./pages/IndexPage";
 
 function App() {
   const [queryClient] = useState(() => new QueryClient());
-  const [trpcClient] = useState(() => trpc.createClient({
-    links: [
-      httpBatchLink({
-        url: "http://localhost:3000"
-      })
-    ]
-  }))
+  const [trpcClient] = useState(() =>
+    trpc.createClient({
+      links: [
+        httpBatchLink({
+          url: "/api/auth",
+        }),
+      ],
+    }),
+  );
 
   return (
     <trpc.Provider client={trpcClient} queryClient={queryClient}>
       <QueryClientProvider client={queryClient}>
-
+        <IndexPage />
       </QueryClientProvider>
     </trpc.Provider>
   );
--- src/apps/frontend/src/pages/IndexPage.tsx
+++ src/apps/frontend/src/pages/IndexPage.tsx
@@ -3,7 +3,7 @@ import { trpc } from "../lib/trpc";
 function IndexPage() {
   const { data, isLoading } = trpc.getHello.useQuery({ name: "World" });
 
-  return (<p>{isLoading ? "Loading..." : data}</p>);
+  return <p>{isLoading ? "Loading..." : data}</p>;
 }
 
 export default IndexPage;
