f7cd57e22a4c9e69e9b319c893c4a2aa486318e1
AntoKeinanen <anto.keinanen@gmail.com>
2025-07-10 20:03:23 +0300

    fix: load env in submodules

--- src/packages/auth-verify/src/env.ts
+++ src/packages/auth-verify/src/env.ts
@@ -1,6 +1,8 @@
 import { createEnv } from "@t3-oss/env-core";
 import { z } from "zod/v4";
 
+import "dotenv/config";
+
 export const env = createEnv({
   server: {
     JWT_ACCESS_TOKEN_SECRET: z.string(),
